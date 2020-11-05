ba6876c660fa3101976462ef4916dba6025afbea
rswalm <62914913+rswalm@users.noreply.github.com>
2020-11-04 23:12:59 -0500

    all code com

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export default
+    {
+        
+        name :'Robin Swalm',
+        headerTagline: [
+                        'Robin Swalm',
+                        'Full Stack Web Developer',
+                        'from Toronto'
+    ],
+        contactEmail:'robin.swalm@gmail.com',
+        resume: "./resume.zip",
+        abouttext: "I love to solve problems. Whether its finding the most elegant way to write a code, or make a website stand out, i love the challenge of discovering a way.",
+        aboutImage:'./2.PNG',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'Progressive Budget',
+            service: 'A Program to Track your finances',
+            imageSrc: "./2.png",
+            url: ' https://github.com/rswalm/RobindBudgetTracker'
+        },
+        {
+            id: 2,
+            title: 'Flyjet',
+            service: 'Search for Flights, flight times, and tickets',
+            imageSrc: "./3.png",
+            url: 'https://github.com/rswalm/flightmaster.git'
+        },
+
+           {
+            id: 3,
+            title:'Employee Management System', 
+             service:'manage departments, roles, and employees', 
+             imageSrc:"./4.png",
+             url:'https://github.com/rswalm/RobinCMDApplication.git'
+            },
+            {
+                id: 4,
+                title: 'Team Profile Generator',
+                service: 'Gather Information About Team Memebers',
+                imageSrc: "./5.png",
+                url: 'https://github.com/rswalm/RobinProfileGenerator.git'
+            },
+            { 
+                id: 5,
+                title: 'Reverse Engineering Authentication',
+                service: 'Reverse Engineering Authentication',
+                imageSrc: "./6.png",
+                url: 'https://docs.google.com/document/d/1lTyQmm_LLEYCCLHv27Y-CWFfY_0fDj-Bz4ejQzi4CO8/edit?usp=sharing'
+            },
+            {
+                id: 6,
+                title: 'Note Taker',
+                service: 'home',
+                imageSrc: "./7.png",
+                url: 'https://robinnotetracker2.herokuapp.com/'
+           }
+
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/rswalm'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/robin-swalm-75b7b91a7/'
+            },
+            {
+                name: 'Gitlab',
+                url: 'https://utoronto.bootcampcontent.com/robinswalm'
+            }
+
+        ]
+    }
