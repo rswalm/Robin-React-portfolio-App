e19a834aff70fdba833c463dc62b19f6858985cb
rswalm <62914913+rswalm@users.noreply.github.com>
2020-11-10 19:49:59 -0500

    Updates

---
+++ precache-manifest.7097c8027f97e60cf4d3826ff380bd8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72cef803c17597425adb5cb437e7285",
+    "url": "/Robin-React-portfolio-App/index.html"
+  },
+  {
+    "revision": "c5a82004d335fe044b1a",
+    "url": "/Robin-React-portfolio-App/static/css/main.445d46c6.chunk.css"
+  },
+  {
+    "revision": "240e413da0035c3716f9",
+    "url": "/Robin-React-portfolio-App/static/js/2.29234ef1.chunk.js"
+  },
+  {
+    "revision": "c5a82004d335fe044b1a",
+    "url": "/Robin-React-portfolio-App/static/js/main.c021e905.chunk.js"
+  },
+  {
+    "revision": "75ce9eb7f3d252cfabb4",
+    "url": "/Robin-React-portfolio-App/static/js/runtime~main.882c37e1.js"
+  },
+  {
+    "revision": "142e178d1beaa73894d6e6ec73233230",
+    "url": "/Robin-React-portfolio-App/static/media/bg.142e178d.PNG"
+  }
+]);
